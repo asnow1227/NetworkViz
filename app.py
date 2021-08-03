19cca5dc9aed5a06a54a329be003de72b5e8a92f
Alexander Snow <alex@alexanders-mbp.myfiosgateway.com>
2021-08-03 08:03:38 -0400

    updated

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def draw_cyto(elements):
                                     }
                                 }],
                             elements=elements,
-                            style={'width': '100%', 'height': '300px'}
+                            style={'width': '100%', 'height': '275px'}
                         )
                     ])
                 ])
